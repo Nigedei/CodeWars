dee84a3501efda44140f625f64613a026b183a51
Nikita <nikita.abermit@stud.hs-ruhrwest.de>
2019-07-20 19:10:03 +0200

    Update: 20.07.2019 - 19:10

--- src/Mid/Starter.java
+++ src/Mid/Starter.java
@@ -2,7 +2,7 @@ package Mid;
 
 public class Starter {
     public static void main(String[] args){
-        EnoughIsEnough enough = new EnoughIsEnough();
-        EnoughIsEnough.deleteNth(new int[]{2,2,2,3,3,3,3,3,3},2);
+        Persist pers = new Persist();
+        System.out.println(pers.persistence(39));
     }
 }
